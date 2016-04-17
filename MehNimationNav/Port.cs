09b2cf04694d1d305d13f4a947689688a2783b8a
David Molinari <contact@davidmolinari.fr>
2016-04-17 18:16:29 +0200

    ajout de la class Port

---
+++ MehNimationNav/Port.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MehNimationNav;
+
+namespace backendPort
+{
+    class Port
+    {
+        private List<Stockage> Stockages;
+        public Port(List<Stockage> Stockages)
+        {
+            this.Stockages = Stockages;
+        }
+       
+        public void dechargement(Navire unNavire)
+        {
+            foreach (var meh in Stockages)
+            {
+                if (unNavire.QteFret >= meh.CapaDispo) { unNavire.Decharger(meh.CapaDispo); meh.Stocker(meh.CapaDispo); Console.WriteLine(meh.CapaDispo); }
+                else { meh.Stocker(meh.CapaDispo); unNavire.Decharger(unNavire.QteFret); }
+            }
+        }
+    }
+}
